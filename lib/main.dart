388080d3cac0f1958b6e3cb146b92ae6a463b71a
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-04-15 11:03:14 -0300

    feat: Implemented flutter_dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:get_it/get_it.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import './controllers/stores/theme_store.dart';
 import './controllers/stores/user_store.dart';
@@ -22,6 +23,7 @@ import './views/screens/history_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   await Firebase.initializeApp();
 
   final GetIt getIt = GetIt.I;
